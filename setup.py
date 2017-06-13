0000791c4aaa1b63d31a289385988c4a5c497fb3
Atsuo Ishimoto <atsuoishimoto@gmail.com>
2017-06-13 12:22:41 +0900

    bump version

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup
 
 setup(
     name = "simple_flask_server",
-    version = "0.0.1",
+    version = "0.0.2",
     author = "Atsuo Ishimoto",
-    author_email = "ishimoto@gembook.org",
+    author_email = "xatsuoi@gmail.com",
     description = "Flask equivalent of python -m SimpleHTTPServer.",
     license = "BSD",
     url = "https://github.com/atsuoishimoto/simple_flask_server",
@@ -17,7 +17,6 @@ setup(
         "Development Status :: 5 - Production/Stable",
         "Topic :: Software Development :: Documentation",
         "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
     ],
 )
